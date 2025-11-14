624a0639ec3480599e92067a6fa130624a27c961
hilda033 <hilinn-4@student.ltu.se>
2025-11-14 14:53:28 +0100

    Added FXML file and configured Launcher to load it.

--- src/main/java/com/pulse/frontend/Launcher.java
+++ src/main/java/com/pulse/frontend/Launcher.java
@@ -2,16 +2,21 @@
 package com.pulse.frontend;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
 public class Launcher extends Application {
     @Override
-    public void start(Stage stage) {
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/RegisterStudentResults.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
         stage.setTitle("Pulse EduDataIntegrator");
-        stage.setScene(new Scene(new Label("Hello, Pulse!"), 420, 200));
+        stage.setScene(scene);
         stage.show();
+        
     }
     public static void main(String[] args) {
         launch(args);
