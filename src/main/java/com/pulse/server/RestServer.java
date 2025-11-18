5edda0392f4b0b5aa6690c797e02841a75d9c3e4
Eynaan <annaemgr@gmail.com>
2025-11-18 10:49:25 +0100

    Added Flyway database migration before server start

--- src/main/java/com/pulse/server/RestServer.java
+++ src/main/java/com/pulse/server/RestServer.java
@@ -1,8 +1,11 @@
 package com.pulse.server;
 
+import org.flywaydb.core.Flyway;
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
+
+import com.pulse.config.DatabaseConfig;
 
 import java.net.URI;
 
@@ -10,12 +13,24 @@ public class RestServer {
 
     private static final String BASE_URI = "http://localhost:8080/api/";
 
+    /**
+     * Run Flyway migrations before starting the HTTP server.
+     */
+    private static void migrateDatabase() {
+        Flyway flyway = Flyway.configure()
+                .dataSource(DatabaseConfig.getUrl(), DatabaseConfig.getUser(), DatabaseConfig.getPassword())
+                .load();
+        flyway.migrate();
+        System.out.println("Flyway database migration completed.");
+    }
+
     public static HttpServer startServer() {
         final ResourceConfig rc = new ResourceConfig().packages("com.pulse.api");
         return GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
     }
 
     public static void main(String[] args) {
+        migrateDatabase();
         final HttpServer server = startServer();
 
         System.out.println("Jersey + Grizzly running at: " + BASE_URI);
